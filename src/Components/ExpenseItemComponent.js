c7a62ddb193dda8e20fd10b97f920a3915f840f3
gichukipitar <ptahgichuki@yahoo.com>
2023-03-14 14:22:28 +0300

    new files

---
+++ src/Components/ExpenseItemComponent.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+import '../Utils/ExpenseItem.css';
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
+
+function ExpenseItemComponent(props) {
+    const [title, setTitle] = useState(props.title);
+
+
+const clickHandler = ()=>{
+setTitle("update");
+};
+
+    return (
+        <Card className="expense-item">
+            <ExpenseDate date={props.date}/>
+            <div className="expense-item__description">
+                <h2>{title}</h2>
+            </div>
+            <div className="expense-item__price">${props.amount}</div>
+            <button onClick={clickHandler}>Change Title</button>
+
+        </Card>
+    );
+
+}
+
+export default ExpenseItemComponent;
---
+++ src/Components/NewExpense/NewExpense.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import ExpenseForm from "./ExpenseForm";
+import '../NewExpense/NewExpense.css'
+
+
+const NewExpense = ()=> {
+
+        return (
+            <div className="new-expense">
+                <ExpenseForm/>
+
+            </div>
+        );
+
+}
+
+export default NewExpense;
